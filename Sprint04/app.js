cc62d607c3dce4afe20908efb28701d3e9d4e163
Kang si yeon <rkdtldus0115@naver.com>
2025-06-26 15:23:55 +0900

    chore: cookie-parser 패키지 설치

--- Sprint04/app.js
+++ Sprint04/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import router from './routes/index.js';
 import { PORT } from './lib/constants.js';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(router);
 
 
