57216c8190f999a95f2f9a0886e89a8edbdd3c6c
Kang si yeon <rkdtldus0115@naver.com>
2025-06-26 13:15:39 +0900

    feature: 회원가입 API 추가, 비밀번호 해싱 저장

---
+++ Sprint04/lib/constants.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000;
+
+export {
+  NODE_ENV,
+  PORT,
+};
---
+++ Sprint04/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import authRouter from './auth.js';
+
+
+const router = express.Router();
+
+router.use(authRouter);
+
+
+export default router;
