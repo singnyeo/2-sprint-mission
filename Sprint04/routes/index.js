1c649b72902ef9a956eb4494186c7ac42f966cdb
Kang si yeon <rkdtldus0115@naver.com>
2025-06-30 09:34:20 +0900

    feature: 유저 API 추가

--- Sprint04/routes/index.js
+++ Sprint04/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRouter from './auth-router.js';
 import productRouter from './product-router.js';
 import articleRouter from './article-router.js';
+import userRouter from './user-router.js';
 
 const router = express.Router();
 
 router.use(authRouter);
 router.use(productRouter);
 router.use(articleRouter);
+router.use(userRouter);
 
 export default router;
