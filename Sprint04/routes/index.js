2304a306c11e84cf034b994c40c08703563b2bd3
Kang si yeon <rkdtldus0115@naver.com>
2025-06-30 09:19:52 +0900

    feature: 게시글 CRUD API 추가

--- Sprint04/routes/index.js
+++ Sprint04/routes/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import authRouter from './auth-router.js';
 import productRouter from './product-router.js';
-
+import articleRouter from './article-router.js';
 
 const router = express.Router();
 
 router.use(authRouter);
 router.use(productRouter);
-
+router.use(articleRouter);
 
 export default router;
